8c2dfc4fecec05a664c22101c3c921d06937df45
Oleksandr Spazhev <o.spazhev@gmail.com>
2023-05-28 10:29:13 +0300

    Create simple web app instead of console app

---
+++ README.md
@@ -0,0 +1,15 @@
+# GSES2 BTC application
+
+## How to run
+Run the next commands to start the app:
+- `docker build -t btc-application .`
+- `docker run -p 8080:8080 btc-application`
+
+App is available on http://localhost:8080
+
+
+
+
+
+
+Author: Spazhev Oleksandr
--- src/main.go
+++ src/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+func helloHandler(writer http.ResponseWriter, _ *http.Request) {
+	message := []byte("Hello, my friend")
+	_, err := writer.Write(message)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 func main() {
-	fmt.Println("Hello, my friend!")
+	log.Println("Starting web app...")
+	http.HandleFunc("/", helloHandler)
+	err := http.ListenAndServe(":8080", nil)
+	log.Fatal(err)
 }
